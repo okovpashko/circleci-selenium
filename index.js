3858bb960373312974834b616e8567d41d6a25ec
Oleksandr Kovpashko <sandro@macpaw.com>
2019-03-07 16:39:00 +0200

    Add simple webdriverio test

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 'use strict';
 
 
-console.log('Test success');
+const { remote } = require('webdriverio');
+
+(async () => {
+  const browser = await remote({
+    logLevel: 'error',
+    hostname: 'localhost',
+    port: 4444,
+    path: '/wd/hub',
+    capabilities: {
+      browserName: 'chrome'
+    }
+  });
+
+  await browser.url('https://webdriver.io');
+
+  const title = await browser.getTitle();
+  console.log('Title was: ' + title);
+
+  await browser.deleteSession();
+})().catch((e) => console.error(e));
