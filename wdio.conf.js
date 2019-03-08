eb28a11301f4f2284c67f2beeb9aec7b821d29fc
Oleksandr Kovpashko <sandro@macpaw.com>
2019-03-08 11:47:29 +0200

    Use primary image w/o browsers
    Fixes Firefox selenium image issues

--- wdio.conf.js
+++ wdio.conf.js
@@ -51,10 +51,10 @@ exports.config = {
       maxInstances: 1,
       browserName: 'chrome',
     },
-    // {
-    //   maxInstances: 1,
-    //   browserName: 'firefox',
-    // },
+    {
+      maxInstances: 1,
+      browserName: 'firefox',
+    },
   ],
   //
   // ===================
